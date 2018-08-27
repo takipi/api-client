6fb0bee3c95255d4038b1cad2b2593128bba8392
Dor Levi <dor.levi@overops.com>
2018-08-27 21:08:39 +0300

    Add Timer data model

---
+++ api-interface/src/main/java/com/takipi/common/api/data/timer/Timer.java
@@ -0,0 +1,12 @@
+package com.takipi.common.api.data.timer;
+
+import java.util.List;
+
+public class Timer {
+	public String id;
+	public String class_name;
+	public String method_name;
+	public long threshold;
+	public List<String> servers;
+	public List<String> applications;
+}
