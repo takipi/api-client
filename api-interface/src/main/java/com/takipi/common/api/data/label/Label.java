48c0ae239277b28240ae7a3d698b5805dde60251
Dor Levi <dor.levi@overops.com>
2018-09-10 11:55:38 +0300

    Add toString/hashCode/equals support to key model entities

--- api-interface/src/main/java/com/takipi/common/api/data/label/Label.java
+++ api-interface/src/main/java/com/takipi/common/api/data/label/Label.java
@@ -4,4 +4,29 @@ public class Label {
 	public String name;
 	public String type;
 	public String color;
+
+	@Override
+	public String toString() {
+		return (name != null ? name : super.toString());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if ((obj == null) || (!(obj instanceof Label))) {
+			return false;
+		}
+
+		Label other = (Label) obj;
+
+		return ((this.name != null) && (other.name != null) && (name.equals(other.name)));
+	}
+
+	@Override
+	public int hashCode() {
+		return (name != null ? name.hashCode() : super.hashCode());
+	}
 }
