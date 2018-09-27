69b892e953ace2ba6c0f6d4c73ce8b564a9d22bf
Dor Levi <dor.levi@overops.com>
2018-09-27 11:58:19 +0300

    Replacing simple volume member with complex stats

--- api-client/src/main/java/com/takipi/common/api/data/metrics/GraphPoint.java
+++
@@ -1,9 +0,0 @@
-package com.takipi.common.api.data.metrics;
-
-import java.util.List;
-
-public class GraphPoint {
-	public String time;
-	public long value;
-	public List<GraphPointContributor> contributors;
-}
--- api-client/src/main/java/com/takipi/common/api/data/metrics/GraphPointContributor.java
+++
@@ -1,6 +0,0 @@
-package com.takipi.common.api.data.metrics;
-
-public class GraphPointContributor {
-	public String id;
-	public long value;
-}
