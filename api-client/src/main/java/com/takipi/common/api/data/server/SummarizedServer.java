7e30900c6d7b46d62ce861e32c1be7a0de2a5d67
Chen Harel <chook.harel@gmail.com>
2018-10-07 14:55:20 +0300

    adding "active" to deployments. adding list servers and list applications apis

---
+++ api-client/src/main/java/com/takipi/common/api/data/server/SummarizedServer.java
@@ -0,0 +1,5 @@
+package com.takipi.common.api.data.server;
+
+public class SummarizedServer {
+	public String name;
+}
---
+++ api-client/src/main/java/com/takipi/common/api/result/server/ServersResult.java
@@ -0,0 +1,10 @@
+package com.takipi.common.api.result.server;
+
+import java.util.List;
+
+import com.takipi.common.api.data.server.SummarizedServer;
+import com.takipi.common.api.result.intf.ApiResult;
+
+public class ServersResult  implements ApiResult {
+	public List<SummarizedServer> servers;
+}
