434750db778e54807b4bab861c21382ad8aad4bc
Dor Levi <dor.levi@overops.com>
2018-10-08 11:47:32 +0300

    Match EventForceSnapshotRequest to api spec

--- api-client/src/main/java/com/takipi/common/api/request/event/EventForceSnapshotRequest.java
+++ api-client/src/main/java/com/takipi/common/api/request/event/EventForceSnapshotRequest.java
@@ -1,19 +1,28 @@
-
 package com.takipi.common.api.request.event;
 
 import java.io.UnsupportedEncodingException;
 
 import com.takipi.common.api.request.ServiceRequest;
 import com.takipi.common.api.request.intf.ApiPostRequest;
-import com.takipi.common.api.result.event.EventResult;
+import com.takipi.common.api.result.EmptyResult;
 
-public class EventForceSnapshotRequest extends ServiceRequest implements ApiPostRequest<EventResult> {
+public class EventForceSnapshotRequest extends ServiceRequest implements ApiPostRequest<EmptyResult> {
 	private final String eventId;
 
 	EventForceSnapshotRequest(String serviceId, String eventId) {
 		super(serviceId);
 
 		this.eventId = eventId;
+	}
+
+	@Override
+	public Class<EmptyResult> resultClass() {
+		return EmptyResult.class;
+	}
+
+	@Override
+	public byte[] postData() throws UnsupportedEncodingException {
+		return null;
 	}
 
 	@Override
@@ -50,16 +59,5 @@ public class EventForceSnapshotRequest extends ServiceRequest implements ApiPost
 
 			return new EventForceSnapshotRequest(serviceId, eventId);
 		}
-	}
-
-	@Override
-	public Class<EventResult> resultClass() {
-		return EventResult.class;
-	}
-
-	@Override
-	public byte[] postData() throws UnsupportedEncodingException {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
