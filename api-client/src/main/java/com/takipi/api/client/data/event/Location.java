9a15bd95b8933cf29a4016adae5b7c4a2b089c45
Chen Harel <chook.harel@gmail.com>
2018-12-02 10:59:28 +0200

    adding method_position to event location

--- api-client/src/main/java/com/takipi/api/client/data/event/Location.java
+++ api-client/src/main/java/com/takipi/api/client/data/event/Location.java
@@ -7,15 +7,15 @@ public class Location {
 	public String class_name;
 	public String method_name;
 	public String method_desc;
+	public int method_position = -1;
 
 	@Override
 	public boolean equals(Object obj) {
-
 		if (!(obj instanceof Location)) {
 			return false;
 		}
 
-		Location other = (Location) obj;
+		Location other = (Location)obj;
 
 		if (!Objects.equal(class_name, other.class_name)) {
 			return false;
@@ -28,18 +28,22 @@ public class Location {
 		if (!Objects.equal(method_desc, other.method_desc)) {
 			return false;
 		}
-
+		
+		if (method_position != other.method_position) {
+			return false;
+		}
+		
 		return true;
 	}
 
 	@Override
 	public int hashCode() {
-
 		StringBuilder result = new StringBuilder();
 
 		result.append(class_name);
 		result.append(method_name);
 		result.append(method_desc);
+		result.append(method_position);
 
 		return result.toString().hashCode();
 	}
