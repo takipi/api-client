2bc9ca6b0989e8c7eac6b1c55ce641142d951864
Dor Levi <dor.levi@overops.com>
2018-12-03 19:53:39 +0200

    Add detailed location to Transaction

--- api-client/src/main/java/com/takipi/api/client/data/transaction/Transaction.java
+++ api-client/src/main/java/com/takipi/api/client/data/transaction/Transaction.java
@@ -2,5 +2,9 @@ package com.takipi.api.client.data.transaction;
 
 public class Transaction {
 	public String name;
+	public String class_name;
+	public String method_name;
+	public String method_desc;
+	
 	public Stats stats;
 }
