e50bd13bf059f54232d0e8f9ae28b99e0ad9f9a3
AvivDanziger <aviv.danziger@takipi.com>
2019-01-17 13:47:45 +0200

    Add get reliability settings request

---
+++ api-client/src/main/java/com/takipi/api/client/result/reliability/GetReliabilitySettingsResult.java
@@ -0,0 +1,7 @@
+package com.takipi.api.client.result.reliability;
+
+import com.takipi.api.core.result.intf.ApiResult;
+
+public class GetReliabilitySettingsResult implements ApiResult {
+	String reliability_settings_json;
+}
