9c268d3054300dc8e4f21ffd887ae2f237e8c9bd
inbal.elkabetz <inbal.elkavetz@takipi.com>
2019-02-25 10:03:02 +0200

    make fields public and rename according to api change

--- api-client/src/main/java/com/takipi/api/client/data/advanced/AdvancedSettings.java
+++ api-client/src/main/java/com/takipi/api/client/data/advanced/AdvancedSettings.java
@@ -4,10 +4,6 @@ import com.takipi.api.core.result.intf.ApiResult;
 
 public class AdvancedSettings implements ApiResult
 {
-	String allowed_ips;
-	Boolean show_rethrows;
-	Boolean show_log_links;
-	Boolean clear_determinant_filters;
 	
 	public AdvancedSettings(String allowed_ips, Boolean show_rethrows, Boolean show_log_links, Boolean clear_determinant_filters)
 	{
@@ -16,4 +12,8 @@ public class AdvancedSettings implements ApiResult
 		this.show_log_links = show_log_links;
 		this.clear_determinant_filters = clear_determinant_filters;
 	}
+	public String allowed_ips;
+	public boolean show_rethrows;
+	public boolean show_log_links;
+	public boolean clear_env_filters;
 }
