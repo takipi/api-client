d4130f4f3be62bba1472810334d3d634aa66f334
inbal.elkabetz <inbal.elkavetz@takipi.com>
2019-02-25 10:03:40 +0200

    remove uneeded c'tor

--- api-client/src/main/java/com/takipi/api/client/data/advanced/AdvancedSettings.java
+++ api-client/src/main/java/com/takipi/api/client/data/advanced/AdvancedSettings.java
@@ -4,14 +4,6 @@ import com.takipi.api.core.result.intf.ApiResult;
 
 public class AdvancedSettings implements ApiResult
 {
-	
-	public AdvancedSettings(String allowed_ips, Boolean show_rethrows, Boolean show_log_links, Boolean clear_determinant_filters)
-	{
-		this.allowed_ips = allowed_ips;
-		this.show_rethrows = show_rethrows;
-		this.show_log_links = show_log_links;
-		this.clear_determinant_filters = clear_determinant_filters;
-	}
 	public String allowed_ips;
 	public boolean show_rethrows;
 	public boolean show_log_links;
