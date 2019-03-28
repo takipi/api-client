07015f8e83f596d98011434996d02be7b2eb03a7
Dor Levi <dor.levi@overops.com>
2019-03-28 11:57:54 +0200

    Add option to request stacktrace in EventRequest

--- api-client/src/main/java/com/takipi/api/client/request/event/EventRequest.java
+++ api-client/src/main/java/com/takipi/api/client/request/event/EventRequest.java
@@ -1,5 +1,7 @@
 package com.takipi.api.client.request.event;
 
+import java.io.UnsupportedEncodingException;
+
 import com.takipi.api.client.request.ServiceRequest;
 import com.takipi.api.client.result.event.EventResult;
 import com.takipi.api.client.util.validation.ValidationUtil;
@@ -7,16 +9,27 @@ import com.takipi.api.core.request.intf.ApiGetRequest;
 
 public class EventRequest extends ServiceRequest implements ApiGetRequest<EventResult> {
 	public final String eventId;
+	public final boolean includeStacktrace;
 
-	EventRequest(String serviceId, String eventId) {
+	EventRequest(String serviceId, String eventId, boolean includeStacktrace) {
 		super(serviceId);
 
 		this.eventId = eventId;
+		this.includeStacktrace = includeStacktrace;
 	}
 
 	@Override
 	public String urlPath() {
 		return baseUrlPath() + "/events/" + eventId;
+	}
+
+	@Override
+	public String[] queryParams() throws UnsupportedEncodingException {
+		String[] params = new String[1];
+
+		params[0] = "stacktrace=" + Boolean.toString(includeStacktrace);
+
+		return params;
 	}
 
 	@Override
@@ -30,6 +43,7 @@ public class EventRequest extends ServiceRequest implements ApiGetRequest<EventR
 
 	public static class Builder extends ServiceRequest.Builder {
 		private String eventId;
+		private boolean includeStacktrace;
 
 		Builder() {
 
@@ -48,6 +62,12 @@ public class EventRequest extends ServiceRequest implements ApiGetRequest<EventR
 			return this;
 		}
 
+		public Builder setIncludeStacktrace(boolean includeStacktrace) {
+			this.includeStacktrace = includeStacktrace;
+
+			return this;
+		}
+
 		@Override
 		protected void validate() {
 			super.validate();
@@ -60,7 +80,7 @@ public class EventRequest extends ServiceRequest implements ApiGetRequest<EventR
 		public EventRequest build() {
 			validate();
 
-			return new EventRequest(serviceId, eventId);
+			return new EventRequest(serviceId, eventId, includeStacktrace);
 		}
 	}
 }
