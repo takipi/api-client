029fe8da8af8e5402a5328b1fb73250e54805f68
Dor Levi <dor.levi@overops.com>
2019-04-12 21:26:38 +0300

    Automatic formatting

--- api-client/src/main/java/com/takipi/api/client/request/metrics/system/SystemMetricGraphRequest.java
+++ api-client/src/main/java/com/takipi/api/client/request/metrics/system/SystemMetricGraphRequest.java
@@ -12,22 +12,22 @@ public class SystemMetricGraphRequest extends TimeframeRequest implements ApiGet
 	public final String metricName;
 	public final int wantedPointCount;
 	public final GraphResolution resolution;
-	
-	private SystemMetricGraphRequest(String serviceId, String metricName, String from, String to,
-			int wantedPointCount, GraphResolution resolution,
-			Collection<String> servers, Collection<String> apps, Collection<String> deployments) {
+
+	SystemMetricGraphRequest(String serviceId, String metricName, String from, String to, int wantedPointCount,
+			GraphResolution resolution, Collection<String> servers, Collection<String> apps,
+			Collection<String> deployments) {
 		super(serviceId, from, to, servers, apps, deployments);
 
 		this.metricName = metricName;
 		this.wantedPointCount = wantedPointCount;
 		this.resolution = resolution;
 	}
-	
+
 	@Override
 	public Class<SystemMetricGraphResult> resultClass() {
 		return SystemMetricGraphResult.class;
 	}
-	
+
 	@Override
 	public String urlPath() {
 		return baseUrlPath() + "/metrics/system/" + metricName + "/graph";
@@ -39,109 +39,109 @@ public class SystemMetricGraphRequest extends TimeframeRequest implements ApiGet
 		//
 		return super.paramsCount() + 1;
 	}
-	
+
 	@Override
 	protected int fillParams(String[] params, int startIndex) throws UnsupportedEncodingException {
 		int index = super.fillParams(params, startIndex);
-		
+
 		if (resolution != null) {
 			params[index++] = "resolution=" + resolution.name();
 		} else {
 			params[index++] = "points=" + String.valueOf(wantedPointCount);
 		}
-		
+
 		return index;
 	}
-	
+
 	@Override
 	public String[] queryParams() throws UnsupportedEncodingException {
 		return buildParams();
 	}
-	
+
 	public static Builder newBuilder() {
 		return new Builder();
 	}
-	
+
 	public static class Builder extends TimeframeRequest.Builder {
 		private String metricName;
 		private int wantedPointCount;
 		private GraphResolution resolution;
-		
+
 		@Override
 		public Builder setServiceId(String serviceId) {
 			super.setServiceId(serviceId);
-			
+
 			return this;
 		}
-		
+
 		public Builder setMetricName(String metricName) {
 			this.metricName = metricName;
-			
+
 			return this;
 		}
-		
+
 		@Override
 		public Builder setFrom(String from) {
 			super.setFrom(from);
-			
+
 			return this;
 		}
 
 		@Override
 		public Builder setTo(String to) {
 			super.setTo(to);
-			
+
 			return this;
 		}
-		
+
 		public Builder setWantedPointCount(int wantedPointCount) {
 			this.wantedPointCount = wantedPointCount;
-			
+
 			return this;
 		}
-		
+
 		public Builder setResolution(GraphResolution resolution) {
 			this.resolution = resolution;
-			
+
 			return this;
 		}
-		
+
 		@Override
 		public Builder addServer(String server) {
 			super.addServer(server);
-			
+
 			return this;
 		}
-		
+
 		@Override
 		public Builder addApp(String app) {
 			super.addApp(app);
-			
+
 			return this;
 		}
-		
+
 		@Override
 		public Builder addDeployment(String deployment) {
 			super.addDeployment(deployment);
-			
+
 			return this;
 		}
-		
+
 		@Override
 		protected void validate() {
 			super.validate();
-			
+
 			if ((resolution == null) && (wantedPointCount <= 0)) {
 				throw new IllegalArgumentException(
 						"Missing graph resolution with illegal wanted point count - " + wantedPointCount);
 			}
 		}
-		
+
 		public SystemMetricGraphRequest build() {
 			validate();
-			
-			return new SystemMetricGraphRequest(serviceId, metricName, from, to,
-					wantedPointCount, resolution, servers, apps, deployments);
+
+			return new SystemMetricGraphRequest(serviceId, metricName, from, to, wantedPointCount, resolution, servers,
+					apps, deployments);
 		}
 	}
 }
