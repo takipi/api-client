839e0a254d4d1893fe61df95abf5f9e12477cec8
Chen Harel <chook.harel@gmail.com>
2019-11-20 14:41:46 -0800

    splash screens

--- api-client-util/src/main/java/com/takipi/api/client/functions/input/FeatureFieldsInput.java
+++ api-client-util/src/main/java/com/takipi/api/client/functions/input/FeatureFieldsInput.java
@@ -8,21 +8,24 @@ import com.takipi.integrations.functions.annotations.Function.FunctionType;
 
 @Function(name="featureFields", type=FunctionType.Variable,
 description = "", example="", image="", isInternal=true)
-public class FeatureFieldsInput extends VariableInput{
-	
+public class FeatureFieldsInput extends VariableInput {
+	public String featureType;
 	public String featureName;
 	public String fieldName;
 	
-	public static final String  NAME = "name";
-	public static final String  TITLE = "title";
-	public static final String  SUB_TITLE= "sub_title";
-	public static final String  TEXT = "text";
-	public static final String  VIDEO_URL = "video_url";
-	public static final String  SCREENSHOT_URL = "screenshot_url";
-	public static final String  DEMO_URL = "demo_url";
-	public static final String  INSTALL_URL= "install_url";
+	public static final String NAME = "name";
+	public static final String TITLE = "title";
+	public static final String SUB_TITLE= "sub_title";
+	public static final String TEXT = "text";
+	public static final String VIDEO_URL = "video_url";
+	public static final String SCREENSHOT_URL = "screenshot_url";
+	public static final String DEMO_URL = "demo_url";
+	public static final String INSTALL_URL= "install_url";
+	
+	public static final String DASHBOARDS = "dashboards";
+	public static final String INTEGRATIONS = "integrations";
 	
 	public static List<String> FIELDS = Arrays.asList(new String[] {
-		NAME	, TITLE, SUB_TITLE, TEXT, SCREENSHOT_URL, VIDEO_URL, DEMO_URL,INSTALL_URL 	
+		NAME, TITLE, SUB_TITLE, TEXT, SCREENSHOT_URL, VIDEO_URL, DEMO_URL, INSTALL_URL 	
 	});
 }
--- api-client-util/src/main/java/com/takipi/api/client/functions/input/FeatureListInput.java
+++ api-client-util/src/main/java/com/takipi/api/client/functions/input/FeatureListInput.java
@@ -6,5 +6,5 @@ import com.takipi.integrations.functions.annotations.Function.FunctionType;
 @Function(name="featureList", type=FunctionType.Variable,
 description = "", example="", image="", isInternal=true)
 public class FeatureListInput extends VariableInput{
-	
+	public String featureType;
 }
